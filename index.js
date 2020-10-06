bf1c0634b912b5405759076b2626be90c99f09f3
royc67 <royc67@gmail.com>
2020-10-06 19:01:18 +0300

    kosomo

--- index.js
+++ index.js
@@ -2,68 +2,80 @@ class MySequelize {
     constructor(connect, tableName) {
         this.connection = connect;
         this.table = tableName;
-    }
 
+        this.optionsHandler = (type ,prop) => {
+            switch(type) {
+                case ('where'): {
+                    let query = "";
+                    for (let key in prop) {
+                        if (query.length>0) query += " AND ";
+                        query += `${key} = '${prop[key]}'`
+                        }
+                    return `WHERE ${query}`
+                    }
+                case ('limit'): {
+                    return `LIMIT ${prop}`
+                }
+                case ('order'): {
+                    return `ORDER BY ${prop.join(' ')}`
+                }
+                case ('attributes'): {
+                    let attributesArr = prop.map(attribute => {
+                        if (typeof attribute === 'string') return attribute;
+                        if (typeof attribute === 'object') return attribute.join(' AS ');
+                    })
+                    return attributesArr.join(', ');
+                }
+                case ('include'): {
+                    return '';    
+                }
+            }    
+        }
+        this.queryBuilder = (options = {}) => {
+            let queries = {}
+            for (let type in options){
+                queries[type] = this.optionsHandler(type, options[type])
+            }
+            return queries;
+        }
+    } 
     async create(obj) {
        const result = await this.connection.query(`INSERT INTO ${this.table} SET ?`, obj)
 
     }
-
     async bulkCreate(arr) {
         const results = arr.map(async (obj) => await this.connection.query(`INSERT INTO ${this.table} SET ?`, obj))
     }
-
     async findAll(options) {
         let results;
+        let queryProps;
 
-        let attributesQuery = '*';
-        let whereQuery = '';
-        let limitQuery = '';
-        let orederByQuery = '';
-        let includeQuery = '';
         if (options){
-            if (options.hasOwnProperty('where')){
-                for (let key in options.where) {
-                    if (whereQuery.length>0) whereQuery += " AND ";
-                    whereQuery += `${key} = '${options.where[key]}'`
-                    }
-                whereQuery = `WHERE ${whereQuery}`
-                }
-            if (options.hasOwnProperty('order')){
-                orederByQuery = `ORDER BY ${options.order.join(' ')}`
+            queryProps = this.queryBuilder(options)
+            results = await this.connection.query(`
+                select ${(queryProps.attributes) ? queryProps.attributes : '*'} 
+                from ${this.table}
+                ${(queryProps.where) ? queryProps.where : ''}
+                ${(queryProps.order) ? queryProps.order : ''}
+                ${(queryProps.limit) ? queryProps.limit : ''}
+                `)
+            if (options.include) { 
+                    const finalIncludedResults = await results[0].map( async (res) => {
+                        const includeRes = await this.connection.query(`
+                        SELECT * FROM ${options.include[0].table}
+                        WHERE ${options.include[0].tableForeignKey}='${res[options.include[0].sourceForeignKey]}'
+                        `)
+                        console.log('includeRes[0]', includeRes[0]);
+                        res.playlists = includeRes[0]
+                        return res;
+                    })
+                    console.log('finalIncludedResults', finalIncludedResults)
+                    return finalIncludedResults;
             }
-            
-            if (options.hasOwnProperty('limit')){
-                limitQuery = `LIMIT ${options.limit}`
-            }
-            
-            if (options.hasOwnProperty('attributes')){
-                let attributesArr = options.attributes.map(attribute => {
-                    if (typeof attribute === 'string') return attribute;
-                    if (typeof attribute === 'object') return attribute.join(' AS ');
-                })
-                attributesQuery = attributesArr.join(', ');
-                // console.log(' att querey: ', attributesQuery)
-            }
-            if (options.hasOwnProperty('include')){
-                const tempRes = await this.connection.query(`SELECT * FROM ${this.table}`)
-                const finalRes = tempRes[0].map(async (res) => {
-                    const includedRes = await this.connection.query(`SELECT * 
-                                                                     FROM ${options.include.table} 
-                                                                     WHERE ${options.include.tableForeignKey}=${this.table}.${res.id}`)
-                    res.playlists = includedRes[0];
-                })
-                return finalRes;
-            }
+        } else {
+            results = await this.connection.query(`SELECT * FROM ${this.table}`)
         }
-        results = await this.connection.query(`
-            select ${attributesQuery} 
-            from ${this.table}
-            ${whereQuery}
-            ${includeQuery}
-            ${orederByQuery}
-            ${limitQuery}
-            `)
+
         return results[0]
     }
     async findByPk(id) {
